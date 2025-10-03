70661abf608e32790db35e5c10658dc715803fee
LoriaRujoy <1306650725@qq.com>
2025-10-04 01:31:26 +0800

    添加：Unity新建C#脚本自动添加头结点相关脚本

---
+++ FishingJoy/FishingJoy/Assets/Plugins/Editor/ScriptsInfoRecoder.cs
@@ -0,0 +1,33 @@
+﻿
+#region 脚本功能、创建时间和文件作者
+/**************************************
+    文件：ScriptsInfoRecoder.cs
+    作者：LoriaRujoy
+    邮箱：2659635618@qq.com
+    时间：2025/8/30 12:00
+    功能：脚本添加头部注释
+***************************************/
+#endregion
+
+using System;
+using System.IO;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ScriptsInfoRecoder : UnityEditor.AssetModificationProcessor {
+    //存在文件还未生成完全就执行该脚本情况，会导致异常脚本创建失败
+    //将OnWillCreateAsset改为OnDidCreateAsset
+    //private static void OnWillCreateAsset(string path) {
+    private static void OnDidCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        if (path.EndsWith(".cs"))
+        {
+            string str = File.ReadAllText(path);
+            str = str.Replace("#CreateAuthor#", Environment.UserName).Replace(
+                              "#CreateTime#", string.Concat(DateTime.Now.Year, "/", DateTime.Now.Month, "/",
+                                DateTime.Now.Day, " ", DateTime.Now.Hour, ":", DateTime.Now.Minute));
+            File.WriteAllText(path, str);
+        }
+    }
+}
