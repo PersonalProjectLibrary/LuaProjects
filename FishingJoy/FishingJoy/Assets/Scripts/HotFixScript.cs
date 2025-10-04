e26386c875136c76497b9408817a520679b9c7d8
LoriaRujoy <1306650725@qq.com>
2025-10-04 16:23:54 +0800

    lua虚拟环境搭建，并整理文档

---
+++ FishingJoy/FishingJoy/Assets/Scripts/HotFixScript.cs
@@ -0,0 +1,42 @@
+
+#region 脚本功能、创建时间和文件作者
+/**************************************
+    文件：HotFixScript.cs
+    作者：LoriaRujoy
+    邮箱：2659635618@qq.com
+    时间：2025-10-04 
+    功能：Lua虚拟环境搭建
+***************************************/
+#endregion
+
+using System.IO;
+using UnityEngine;
+using XLua;
+
+public class HotFixScript : MonoBehaviour
+{
+    private LuaEnv luaEnv;
+    private string filePath;
+    void Start()
+    {
+        luaEnv = new LuaEnv();
+        //相对路径，Asset同级目录下的LuaScripts文件夹
+        filePath = Application.dataPath + "/../LuaScripts/";
+        //加载Lua文件
+        luaEnv.AddLoader(MyLoader);
+        //运行Lua文件
+        luaEnv.DoString("require 'Fish'");
+    }
+
+    private byte[] MyLoader(ref string luaFilePath)
+    {
+        string localFile = filePath + luaFilePath + ".lua.txt";
+        return System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(localFile));
+    }
+
+    private void OnDestroy()
+    {
+        //销毁Lua虚拟环境
+        luaEnv.Dispose();
+    }
+}
